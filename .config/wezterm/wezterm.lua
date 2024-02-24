b0832ee29f7d93a29d4aee1ea526de838851854c
Zukkini Kakkokari <zukkini.kakkokari@gmail.com>
2024-02-24 10:43:29 +0300

    init

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font_size = 25
+config.font = wezterm.font 'Iosevka Nerd Font'
+config.adjust_window_size_when_changing_font_size = false
+config.enable_tab_bar = false
+config.exit_behavior = 'Close'
+
+
+
+local act = wezterm.action
+
+config.keys = {
+  -- { key = 'u', mods = 'CTRL', action = act.ScrollByPage(-0.5) },
+  -- { key = 'd', mods = 'CTRL', action = act.ScrollByPage(0.5) },
+  { key = 'k', mods = 'CTRL', action = act.ScrollByLine(-5) },
+  { key = 'j', mods = 'CTRL', action = act.ScrollByLine(5) },
+
+}
+
+
+-- and finally, return the configuration to wezterm
+return config
